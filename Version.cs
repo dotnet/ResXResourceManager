3896c1a856819f48327a179414b0e9da11051d58
tom-englert <mail@tom-englert.de>
2015-05-02 10:35:29 +0000

    V 1.0.0.54"
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS36]$/ResXResourceManager/Main;C35476

--- Version.cs
+++ Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.53")]
-[assembly: AssemblyFileVersion("1.0.0.53")]
+[assembly: AssemblyVersion("1.0.0.54")]
+[assembly: AssemblyFileVersion("1.0.0.54")]
