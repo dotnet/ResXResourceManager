47c61a712d34e8242361207c378b92d3f46cab47
tom-englert <mail@tom-englert.de>
2015-12-19 14:24:17 +0000

    V 1.0.0.66
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS36]$/ResXResourceManager/Main;C36801

--- ResXManager.VSIX/ResXManager.VSIXPackage.cs
+++ ResXManager.VSIX/ResXManager.VSIXPackage.cs
@@ -23,7 +23,7 @@
     // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
     // This attribute is used to register the informations needed to show the this package in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration(@"#110", @"#112", @"1.0.0.65", IconResourceID = 400)]
+    [InstalledProductRegistration(@"#110", @"#112", @"1.0.0.66", IconResourceID = 400)]
     // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource(@"Menus.ctmenu", 1)]
     // This attribute registers a tool window exposed by this package.
--- Version.cs
+++ Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.65")]
-[assembly: AssemblyFileVersion("1.0.0.65")]
+[assembly: AssemblyVersion("1.0.0.66")]
+[assembly: AssemblyFileVersion("1.0.0.66")]
