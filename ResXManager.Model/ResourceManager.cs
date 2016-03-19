f35f2c3a9b48dfdc38bedc444953dadb94abaf26
tom-englert <mail@tom-englert.de>
2016-03-19 12:34:28 +0000

    Update Packages
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS36]$/ResXResourceManager/Main;C37031

--- ResXManager.Model/ResourceManager.cs
+++ ResXManager.Model/ResourceManager.cs
@@ -58,6 +58,16 @@
             _tracer = tracer;
             _sourceFilesProvider = sourceFilesProvider;
             _resourceTableEntries = _selectedEntities.ObservableSelectMany(entity => entity.Entries);
+            _resourceTableEntries.CollectionChanged += _resourceTableEntries_CollectionChanged;
+            _resourceTableEntries.PropertyChanged += _resourceTableEntries_PropertyChanged;
+        }
+
+        private void _resourceTableEntries_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+        }
+
+        private void _resourceTableEntries_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
         }
 
         /// <summary>
