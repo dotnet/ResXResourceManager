eec8c2d1f875b1663433a28885499802bf3617e6
tom-englert <mail@tom-englert.de>
2016-04-02 10:52:06 +0000

    Show a wait cursor while loading/updating the grid
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS36]$/ResXResourceManager/Main;C37062

--- ResXManager.View/Visuals/ShellViewModel.cs
+++ ResXManager.View/Visuals/ShellViewModel.cs
@@ -1,11 +1,51 @@
 ﻿namespace tomenglertde.ResXManager.View.Visuals
 {
-    using tomenglertde.ResXManager.Infrastructure;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics.Contracts;
+    using System.Windows.Threading;
 
+    using tomenglertde.ResXManager.Infrastructure;
+    using tomenglertde.ResXManager.Model;
+
+    using TomsToolbox.Desktop;
+    using TomsToolbox.Wpf;
     using TomsToolbox.Wpf.Composition;
 
     [VisualCompositionExport(RegionId.Shell)]
-    class ShellViewModel 
+    class ShellViewModel : ObservableObject
     {
+        private readonly DispatcherThrottle _updateThrottle;
+        private bool _isLoading;
+
+        [ImportingConstructor]
+        public ShellViewModel(ResourceManager resourceManager)
+        {
+            Contract.Requires(resourceManager != null);
+
+            _updateThrottle = new DispatcherThrottle(DispatcherPriority.Background, () => IsLoading = false);
+
+            resourceManager.SelectedEntities.CollectionChanged += SelectedEntities_CollectionChanged;
+        }
+
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            set
+            {
+                SetProperty(ref _isLoading, value, nameof(IsLoading));
+            }
+        }
+
+        private void SelectedEntities_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            _updateThrottle.Tick();
+
+            IsLoading = true;
+
+            Dispatcher.ProcessMessages(DispatcherPriority.Render);
+        }
     }
 }
