534da2fcc02fc1b3c579ad40ddd238278b3474aa
tom-englert <mail@tom-englert.de>
2016-05-21 10:19:24 +0000

    Improve speed of code reference tracker analysis as suggested by Pavel Kotrč
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS36]$/ResXResourceManager/Main;C37170

--- ResXManager.Model/CodeReferenceTracker.cs
+++ ResXManager.Model/CodeReferenceTracker.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel.Composition;
+    using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.Diagnostics.Contracts;
     using System.IO;
@@ -12,6 +13,10 @@
     using System.Threading;
 
     using tomenglertde.ResXManager.Infrastructure;
+
+    using TomsToolbox.Core;
+
+    using ThreadState = System.Threading.ThreadState;
 
     [Export]
     public class CodeReferenceTracker
@@ -65,36 +70,40 @@
             Contract.Requires(projectFiles != null);
             Contract.Requires(resourceTableEntries != null);
 
+            var stopwatch = Stopwatch.StartNew();
+
             try
             {
-                foreach (var entry in resourceTableEntries)
-                {
-                    Contract.Assume(entry != null);
-                    entry.CodeReferences = null;
-                }
+                resourceTableEntries.ForEach(entry => entry.CodeReferences = null);
 
-                var sourceFiles = projectFiles.Select(file => new FileInfo(file, configuration.Items))
-                    .Where(file => file.Configurations.Any())
+                var keys = new HashSet<string>(resourceTableEntries.Select(entry => entry.Key));
+
+                var sourceFiles = projectFiles.AsParallel()
+                    .Select(file => new FileInfo(file, configuration.Items, keys))
+                    .Where(file => file.HasConfigurations)
                     .ToArray();
 
-                _total = sourceFiles.Select(sf => sf.Lines.Length).Sum() * resourceTableEntries.Count;
-                _visited = 0;
+                Contract.Assume(sourceFiles != null); // no contracts in Parallel yet...
 
-                var entriesByBaseName = resourceTableEntries.GroupBy(entry => entry.Container.BaseName);
+                var entriesByBaseName = resourceTableEntries
+                    .GroupBy(entry => entry.Container.BaseName)
+                    .ToArray();
+
+                _visited = 0;
+                _total = entriesByBaseName.Sum(entry => entry.Count()) * sourceFiles.Length;
 
                 foreach (var entriesGroup in entriesByBaseName.AsParallel())
                 {
                     Contract.Assume(entriesGroup != null);
+
                     var baseName = entriesGroup.Key;
-                    Contract.Assume(baseName != null);
                     var tableEntries = entriesGroup.ToArray();
 
-                    foreach (var sourceFile in sourceFiles)
+                    sourceFiles.ForEach(sourceFile => tableEntries.ForEach(entry =>
                     {
-                        Contract.Assume(sourceFile != null);
-
-                        FindCodeReferences(sourceFile, baseName, tableEntries, tracer);
-                    }
+                        Interlocked.Increment(ref _visited);
+                        sourceFile.FindCodeReferences(baseName, entry, tracer);
+                    }));
                 }
 
                 foreach (var entry in resourceTableEntries.Where(entry => entry.CodeReferences == null))
@@ -103,73 +112,17 @@
                     // Show 0 code references in UI
                     entry.CodeReferences = new CodeReference[0];
                 }
+
+                Contract.Assume(_visited == _total);
+
+                Debug.WriteLine(stopwatch.Elapsed);
             }
             catch (ThreadAbortException)
             {
             }
-        }
-
-        private void FindCodeReferences(FileInfo source, string baseName, IList<ResourceTableEntry> entries, ITracer tracer)
-        {
-            Contract.Requires(source != null);
-            Contract.Requires(baseName != null);
-            Contract.Requires(entries != null);
-
-            try
+            finally
             {
-                var projectFile = source.ProjectFile;
-                var configurations = source.Configurations;
-
-                foreach (var entry in entries)
-                {
-                    Contract.Assume(entry != null);
-                    var key = entry.Key;
-
-                    var parameters = configurations.Select(cfg => new
-                    {
-                        StringComparison = cfg.IsCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase,
-                        Regex = !string.IsNullOrEmpty(cfg.Expression) ? new Regex(cfg.Expression.Replace("$Key", key).Replace("$File", baseName)) : null,
-                        cfg.SingleLineComment
-                    }).ToArray();
-
-                    var lineNumber = 0;
-
-                    foreach (var line in source.Lines)
-                    {
-                        Contract.Assume(line != null);
-                        lineNumber += 1;
-
-                        Interlocked.Increment(ref _visited);
-
-                        if (line.IndexOf(key, StringComparison.OrdinalIgnoreCase) == -1)
-                            continue;
-
-                        foreach (var parameter in parameters)
-                        {
-                            Contract.Assume(parameter != null);
-                            try
-                            {
-                                var match = new CodeMatch(line, key, parameter.Regex, parameter.StringComparison, parameter.SingleLineComment);
-                                if (!match.Success)
-                                    continue;
-
-                                var codeReference = new CodeReference(projectFile, lineNumber, match.Segments);
-
-                                var codeReferences = entry.CodeReferences ?? (entry.CodeReferences = new ObservableCollection<CodeReference>());
-                                codeReferences.Add(codeReference);
-                                break;
-                            }
-                            catch (Exception ex) // Should not happen, but was reported by someone.
-                            {
-                                tracer.TraceError("Error detecting code reference in file {0}, line {1} for {2}.{3}\n{4}", projectFile.FilePath, lineNumber, baseName, key, ex);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex) // Should not happen, but was reported by someone.
-            {
-                tracer.TraceError("Error detecting code reference in file {0} for {1}\n{2}", source.ProjectFile.FilePath, baseName, ex);
+                stopwatch.Stop();
             }
         }
 
@@ -261,46 +214,102 @@
 
         private class FileInfo
         {
-            private string[] _lines;
+            private static readonly Regex _regex = new Regex(@"\W+", RegexOptions.Compiled);
+            private readonly ProjectFile _projectFile;
+            private readonly string[] _lines;
+            private readonly Dictionary<string, HashSet<int>> _keyLines = new Dictionary<string, HashSet<int>>();
+            private readonly CodeReferenceConfigurationItem[] _configurations;
 
-            public FileInfo(ProjectFile projectFile, IEnumerable<CodeReferenceConfigurationItem> configurations)
+            public FileInfo(ProjectFile projectFile, IEnumerable<CodeReferenceConfigurationItem> configurations, ICollection<string> keys)
             {
                 Contract.Requires(projectFile != null);
                 Contract.Requires(configurations != null);
 
-                ProjectFile = projectFile;
-                Configurations = configurations
+                _projectFile = projectFile;
+
+                _configurations = configurations
                     .Where(item => item.ParseExtensions().Contains(projectFile.Extension, StringComparer.OrdinalIgnoreCase))
                     .ToArray();
+
+                if (!_configurations.Any())
+                    return;
+
+                _lines = _projectFile.ReadAllLines();
+
+                _lines.ForEach((line, index) =>
+                    _regex.Split(line)
+                        .Where(keys.Contains)
+                        .ForEach(key => _keyLines.ForceValue(key, _ => new HashSet<int>()).Add(index)));
             }
 
-            public ProjectFile ProjectFile
-            {
-                get;
-                private set;
-            }
+            public bool HasConfigurations => _configurations.Any();
 
-            public string[] Lines
+
+            public void FindCodeReferences(string baseName, ResourceTableEntry entry, ITracer tracer)
             {
-                get
+                Contract.Requires(baseName != null);
+                Contract.Requires(entry != null);
+
+                try
                 {
-                    Contract.Ensures(Contract.Result<string[]>() != null);
-                    return _lines ?? (_lines = ProjectFile.ReadAllLines());
-                }
-            }
+                    if (_lines == null)
+                        return;
 
-            public CodeReferenceConfigurationItem[] Configurations
-            {
-                get;
-                private set;
+                    var key = entry.Key;
+
+                    var lineIndices = _keyLines.GetValueOrDefault(key);
+                    if (lineIndices == null)
+                        return;
+
+                    var parameters = _configurations.Select(cfg => new
+                    {
+                        StringComparison = cfg.IsCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase,
+                        Regex = !string.IsNullOrEmpty(cfg.Expression) ? new Regex(cfg.Expression.Replace("$Key", key).Replace("$File", baseName)) : null,
+                        cfg.SingleLineComment
+                    }).ToArray();
+
+
+                    lineIndices.ForEach(index =>
+                    {
+                        var line = _lines[index];
+                        var lineNumber = index + 1;
+
+                        foreach (var parameter in parameters)
+                        {
+                            Contract.Assume(parameter != null);
+
+                            try
+                            {
+                                var match = new CodeMatch(line, key, parameter.Regex, parameter.StringComparison, parameter.SingleLineComment);
+                                if (!match.Success)
+                                    continue;
+
+                                var codeReference = new CodeReference(_projectFile, lineNumber, match.Segments);
+
+                                var codeReferences = entry.CodeReferences ?? (entry.CodeReferences = new ObservableCollection<CodeReference>());
+                                codeReferences.Add(codeReference);
+                                break;
+                            }
+                            catch (Exception ex) // Should not happen, but was reported by someone.
+                            {
+                                tracer.TraceError("Error detecting code reference in file {0}, line {1} for {2}.{3}\n{4}", _projectFile.FilePath, lineNumber, baseName, key, ex);
+                            }
+                        }
+                    });
+
+                }
+                catch (Exception ex) // Should not happen, but was reported by someone.
+                {
+                    tracer.TraceError("Error detecting code reference in file {0} for {1}\n{2}", _projectFile.FilePath, baseName, ex);
+                }
             }
 
             [ContractInvariantMethod]
             [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Required for code contracts.")]
             private void ObjectInvariant()
             {
-                Contract.Invariant(ProjectFile != null);
-                Contract.Invariant(Configurations != null);
+                Contract.Invariant(_projectFile != null);
+                Contract.Invariant(_configurations != null);
             }
         }
     }
@@ -324,11 +333,12 @@
         public IList<string> LineSegments { get; private set; }
     }
 
-    static class CodeReferenceExtensionMethods
+    internal static class CodeReferenceExtensionMethods
     {
         public static string[] ReadAllLines(this ProjectFile file)
         {
             Contract.Requires(file != null);
+
             try
             {
                 Thread.Sleep(1);
