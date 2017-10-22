73027de9e66d13d89eadc56eaac51a1aa11a1699
tom-englert <mail@tom-englert.de>
2017-10-22 17:27:58 +0200

    Remove obsolete comparable implementation

--- ResXManager.Model/ResourceEntity.cs
+++ ResXManager.Model/ResourceEntity.cs
@@ -23,7 +23,7 @@
     /// For windows store apps "de\Resources.resw", "en-us\Resources.resw" are also supported.
     /// </summary>
     [AddINotifyPropertyChangedInterface]
-    public sealed class ResourceEntity : IComparable<ResourceEntity>, IComparable, IEquatable<ResourceEntity>
+    public sealed class ResourceEntity
     {
         [NotNull]
         private readonly IDictionary<CultureKey, ResourceLanguage> _languages;
@@ -46,7 +46,6 @@
             _languages = GetResourceLanguages(files);
             RelativePath = GetRelativePath(files);
             DisplayName = projectName + @" - " + RelativePath + baseName;
-            SortKey = string.Concat(@" - ", DisplayName, DirectoryName);
             NeutralProjectFile = files.FirstOrDefault(file => file.GetCultureKey() == CultureKey.Neutral);
 
             var entriesQuery = _languages.Values
@@ -376,124 +375,6 @@
             return hasChanges;
         }
 
-        #region IComparable/IEquatable implementation
-
-        [NotNull]
-        private string SortKey { get; }
-
-        /// <summary>
-        /// Returns a hash code for this instance.
-        /// </summary>
-        /// <returns>
-        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            return SortKey.GetHashCode();
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
-        /// </summary>
-        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
-        /// <returns><c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.</returns>
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as ResourceEntity);
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="ResourceEntity" /> is equal to this instance.
-        /// </summary>
-        /// <param name="other">The <see cref="ResourceEntity"/> to compare with this instance.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified <see cref="ResourceEntity" /> is equal to this instance; otherwise, <c>false</c>.
-        /// </returns>
-        public bool Equals(ResourceEntity other)
-        {
-            return Compare(this, other) == 0;
-        }
-
-        /// <summary>
-        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
-        /// </summary>
-        /// <param name="obj">An object to compare with this instance.</param>
-        public int CompareTo(object obj)
-        {
-            return Compare(this, obj as ResourceEntity);
-        }
-
-        /// <summary>
-        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
-        /// </summary>
-        /// <param name="other">An object to compare with this instance.</param>
-        public int CompareTo(ResourceEntity other)
-        {
-            return Compare(this, other);
-        }
-
-        private static int Compare(ResourceEntity left, ResourceEntity right)
-        {
-            if (ReferenceEquals(left, right))
-                return 0;
-            if (ReferenceEquals(left, null))
-                return -1;
-            if (ReferenceEquals(right, null))
-                return 1;
-
-            return string.Compare(left.SortKey, right.SortKey, StringComparison.OrdinalIgnoreCase);
-        }
-
-        /// <summary>
-        /// Implements the operator ==.
-        /// </summary>
-        public static bool operator ==(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) == 0;
-        }
-
-        /// <summary>
-        /// Implements the operator !=.
-        /// </summary>
-        public static bool operator !=(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) != 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &gt;.
-        /// </summary>
-        public static bool operator >(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) > 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &lt;.
-        /// </summary>
-        public static bool operator <(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) < 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &gt;=.
-        /// </summary>
-        public static bool operator >=(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) >= 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &lt;=.
-        /// </summary>
-        public static bool operator <=(ResourceEntity left, ResourceEntity right)
-        {
-            return Compare(left, right) <= 0;
-        }
-
-        #endregion
-
         [ContractInvariantMethod]
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Required for code contracts.")]
         [Conditional("CONTRACTS_FULL")]
@@ -508,7 +389,6 @@
             Contract.Invariant(!string.IsNullOrEmpty(DirectoryName));
             Contract.Invariant(DisplayName != null);
             Contract.Invariant(RelativePath != null);
-            Contract.Invariant(SortKey != null);
         }
     }
 }
