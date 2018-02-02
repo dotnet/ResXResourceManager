e4f6636ab0d9335f2fc438ee55c9badfc838c7ef
tom-englert <mail@tom-englert.de>
2018-02-02 17:04:07 +0100

    Update copyright to 2018

--- Trademark.cs
+++ Trademark.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("tom-englert.de")]
 [assembly: AssemblyProduct("ResXManager")]
-[assembly: AssemblyCopyright("Copyright © tom-englert.de 2012-2017. Distributed under the MIT License.")]
+[assembly: AssemblyCopyright("Copyright © tom-englert.de 2012-2018. Distributed under the MIT License.")]
 [assembly: AssemblyTrademark("")]
