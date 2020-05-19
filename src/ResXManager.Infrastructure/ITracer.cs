1f6aa6e3018ddc13f905cb8a0c29ca66f06877db
tom-englert <mail@tom-englert.de>
2020-05-19 09:27:09 +0200

    Adjust github links

--- src/ResXManager.Infrastructure/ITracer.cs
+++ src/ResXManager.Infrastructure/ITracer.cs
@@ -99,7 +99,7 @@
                 exportProvider.WriteLine("Duplicate assemblies found: " + string.Join(", ", assembliesByName));
             }
 
-            exportProvider.WriteLine("Please read https://github.com/tom-englert/ResXResourceManager/wiki/Fixing-errors before creating an issue.");
+            exportProvider.WriteLine("Please read https://github.com/dotnet/ResXResourceManager/wiki/Fixing-errors before creating an issue.");
         }
     }
 }
