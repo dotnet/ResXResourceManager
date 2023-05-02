4220f26d8ff8cac1dbacc021410822056f845d6c
Joakim Riedel <joakim.riedel@gmail.com>
2023-05-02 14:22:13 +0200

    add VSIX support for arm64

--- src/ResXManager.VSIX/VSPackage.cs
+++ src/ResXManager.VSIX/VSPackage.cs
@@ -180,9 +180,14 @@
             //    .ToList();
 
             var is64BitProcess = Environment.Is64BitProcess;
+            var isArmArchitecture = RuntimeInformation.ProcessArchitecture == Architecture.Arm64;
 
             using var resourceStream = is64BitProcess
-                ? Resource.AsStream("ResXManager.VSIX.Compatibility.x64.dll")
+                ? (
+                    isArmArchitecture
+                    ? Resource.AsStream("ResXManager.VSIX.Compatibility.arm64.dll")
+                    : Resource.AsStream("ResXManager.VSIX.Compatibility.x64.dll")
+                    )
                 : Resource.AsStream("ResXManager.VSIX.Compatibility.x86.dll");
 
             var length = resourceStream.Length;
