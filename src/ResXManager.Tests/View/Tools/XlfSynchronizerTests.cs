cc42b5d7a393e60e14b9e029fce35898529b1d5d
Geert van Horrik <geert@catenalogic.com>
2023-07-31 20:52:59 +0200

    Add unit tets to reproduce the empty line issue

--- src/ResXManager.Tests/View/Tools/XlfSynchronizerTests.cs
+++ src/ResXManager.Tests/View/Tools/XlfSynchronizerTests.cs
@@ -15,8 +15,10 @@
         [UsesVerify]
         public class The_UpdateEntityFromXlf_Method
         {
-            [Fact]
-            public async Task Does_Not_Add_Empty_Lines()
+            [Theory]
+            [InlineData("fr")]
+            [InlineData("nl")]
+            public async Task Does_Not_Add_Empty_Lines(string languageToVerify)
             {
                 var directory = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "ResXManager.Tests", "XlfSynchronizerTests", "Does_Not_Add_Empty_Lines");
 
@@ -26,7 +28,11 @@
                 var tracerMock = new Mock<ITracer>();
 
                 var projectFileName = Path.Combine(directory, "MyProject.resx");
+                var projectFrFileName = Path.Combine(directory, "MyProject.fr.resx");
                 var projectNlFileName = Path.Combine(directory, "MyProject.nl.resx");
+
+                var fileNameToVerify = Path.Combine(directory, $"MyProject.{languageToVerify}.resx");
+                var xlfDocumentPath = Path.Combine(directory, $"MyProject.{languageToVerify}.xlf");
 
                 // Synchronize 3 times to see if generates the same file
                 for (int i = 0; i < 3; i++)
@@ -36,15 +42,16 @@
                     var projectFiles = new[]
                     {
                         new ProjectFile(projectFileName, directory, "MyProject", null),
+                        new ProjectFile(projectFrFileName, directory, "MyProject", null),
                         new ProjectFile(projectNlFileName, directory, "MyProject", null)
                     };
 
                     var resourceEntity = new ResourceEntity(resourceManager, "MyProject", "MyProject", directory,
-                        projectFiles, new System.Globalization.CultureInfo("nl"), DuplicateKeyHandling.Fail);
+                        projectFiles, new System.Globalization.CultureInfo("en"), DuplicateKeyHandling.Fail);
 
                     resourceManager.ResourceEntities.Add(resourceEntity);
 
-                    var xlfDocument = new XlfDocument(Path.Combine(directory, "MyProject.xlf"));
+                    var xlfDocument = new XlfDocument(xlfDocumentPath);
 
                     using (var xlfSynchronizer = new XlfSynchronizer(resourceManager, tracerMock.Object, configurationMock.Object))
                     {
@@ -54,7 +61,8 @@
                     resourceManager.Save();
                 }
 
-                await Verifier.VerifyFile(projectNlFileName);
+                await Verifier.VerifyFile(fileNameToVerify)
+                    .UseParameters(languageToVerify);
             }
         }
     }
