b2f5a8d5c3ce0ef23c571829e0284db929346f06
etherfield <no-reply@mail>
2023-10-26 21:45:56 +0300

    Support for overriding flags

--- src/ResXManager.View/Converters/CultureToImageSourceConverter.cs
+++ src/ResXManager.View/Converters/CultureToImageSourceConverter.cs
@@ -23,15 +23,15 @@
             "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf",
             "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci",
             "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg",
-            "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp",
-            "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "in", "io", "iq", "ir", "is",
-            "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr",
-            "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt",
-            "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe",
-            "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc",
-            "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th",
-            "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg",
-            "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"
+            "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "fy-nl",    "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm",
+            "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "in", "io", "iq",
+            "ir", "is", "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li",
+            "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr",
+            "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om",
+            "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa",
+            "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf",
+            "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc",
+            "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"
         };
 
         [ImportingConstructor]
@@ -56,24 +56,29 @@
         {
             culture ??= _configuration.NeutralResourcesLanguage;
 
-            var cultureName = culture.Name;
+            var key = culture.Name;
 
-            if (culture.IsNeutralCulture)
+            if (!ExactCultureOverrides.Exact.HasCustomFlag(culture))
             {
-                var countryOverride = NeutralCultureCountryOverrides.Default[culture];
+                var cultureName = culture.Name;
 
-                if (countryOverride != null)
+                if (culture.IsNeutralCulture)
                 {
-                    culture = countryOverride;
-                    cultureName = culture.Name;
+                    var countryOverride = NeutralCultureCountryOverrides.Default[culture];
+
+                    if (countryOverride != null)
+                    {
+                        culture = countryOverride;
+                        cultureName = culture.Name;
+                    }
                 }
+
+                var cultureParts = cultureName.Split('-');
+                if (!cultureParts.Any())
+                    return null;
+
+                key = cultureParts.Last();
             }
-
-            var cultureParts = cultureName.Split('-');
-            if (!cultureParts.Any())
-                return null;
-
-            var key = cultureParts.Last();
 
             if (Array.BinarySearch(_existingFlags, key, StringComparer.OrdinalIgnoreCase) < 0)
             {
---
+++ src/ResXManager.View/Tools/ExactCultureOverrides.cs
@@ -0,0 +1,39 @@
+﻿namespace ResXManager.View.Tools
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using TomsToolbox.Essentials;
+
+    internal class ExactCultureOverrides
+    {
+        private const string DefaultOverrides = "fy-NL,";
+
+        private readonly HashSet<CultureInfo> _overrides = ReadDefault().ToHashSet(_comparer);
+        private static readonly IEqualityComparer<CultureInfo> _comparer = new DelegateEqualityComparer<CultureInfo>();
+        public static readonly ExactCultureOverrides Exact = new();
+
+        private ExactCultureOverrides()
+        {
+        }
+
+        /// <summary>
+        /// Checks if there should be a custom flag for the specific culture. Doesn't check actual file existence
+        /// </summary>
+        public bool HasCustomFlag(CultureInfo culture)
+        {
+            return _overrides.Contains(culture);
+        }
+
+        private static IEnumerable<CultureInfo> ReadDefault()
+        {
+            var exactCultureOverrides = DefaultOverrides.Split(',').Where(item => !string.IsNullOrEmpty(item));
+
+            foreach (var item in exactCultureOverrides)
+            {
+                yield return CultureInfo.GetCultureInfo(item);
+            }
+        }
+    }
+}
