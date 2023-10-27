bc76c6770fae674f1344a19aa2e4e3780da01fb1
etherfield <148793545+etherfield@users.noreply.github.com>
2023-10-27 14:17:41 +0300

    Unit tests

---
+++ src/ResXManager.Tests/View/Converters/CultureToImageSourceConverterTests.cs
@@ -0,0 +1,58 @@
+﻿namespace ResXManager.Tests.View.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+
+using NSubstitute;
+
+using ResXManager.Model;
+using ResXManager.View.Converters;
+using ResXManager.View.Tools;
+
+using Xunit;
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+public static class CultureToImageSourceConverterTests
+{
+    public class The_Convert_Method
+    {
+        [Theory]
+        [InlineData("fy-NL", "fy-NL.gif")]
+        [InlineData("fy", "fy-NL.gif")]
+        [InlineData("en", "us.gif")]
+        [InlineData("de-DE", "de.gif")]
+        public void Should_Produce_Image_For_Exact_And_Overriden_Culture_Match(string culture, string expectedImageName)
+        {
+            var configurationMock = Substitute.For<IConfiguration>();
+            var converter = new CultureToImageSourceConverter(configurationMock);
+
+            var cultureImage = converter.Convert(CultureInfo.GetCultureInfo(culture)) as BitmapImage;
+
+            Assert.NotNull(cultureImage);
+            Assert.EndsWith(expectedImageName, cultureImage.UriSource.OriginalString, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Theory]
+        [InlineData("", "", "fr", "fr.gif")]
+        [InlineData("", "", "fr-CD", "cd.gif")]
+        [InlineData("ar", "ar-SA", "ar", "sa.gif")]
+        [InlineData("ms", "ms-BN", "ms-BN", "bn.gif")]
+        public void Should_Produce_Overriden_Culture_Image_If_Overriden_With_Settings(string cultureFrom, string cultureTo,
+            string culture, string expectedImageName)
+        {
+            var configurationMock = Substitute.For<IConfiguration>();
+            if (!string.IsNullOrEmpty(cultureFrom) && !string.IsNullOrEmpty(cultureTo) )
+            {
+                NeutralCultureCountryOverrides.Default[CultureInfo.GetCultureInfo(cultureFrom)] = CultureInfo.GetCultureInfo(cultureTo);
+            }
+
+            var converter = new CultureToImageSourceConverter(configurationMock);
+            var cultureImage = converter.Convert(CultureInfo.GetCultureInfo(culture)) as BitmapImage;
+
+            Assert.NotNull(cultureImage);
+            Assert.EndsWith(expectedImageName, cultureImage.UriSource.OriginalString, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ src/ResXManager.Tests/View/Tools/ExactCultureOverridesTests.cs
@@ -0,0 +1,24 @@
+﻿namespace ResXManager.Tests.View.Tools;
+
+using System.Globalization;
+
+using ResXManager.View.Tools;
+
+using Xunit;
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+public static class ExactCultureOverridesTests
+{
+    public class The_ReadDefault_Method
+    {
+        [Fact]
+        public void Initializes_Cultures_Correctly()
+        {
+            var action = () => ExactCultureOverrides.Exact.HasCustomFlag(CultureInfo.CurrentCulture);
+
+            var exception = Record.Exception(() => action());
+            Assert.Null(exception);
+        }
+    }
+}
