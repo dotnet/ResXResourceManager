c8939d9b54b31e0a56974c439d359b9621302b97
etherfield <148793545+etherfield@users.noreply.github.com>
2023-11-03 22:34:07 +0300

    Automatic suggestion on language selection

---
+++ src/ResXManager.View/Controls/AutoSuggestBox.cs
@@ -0,0 +1,46 @@
+﻿namespace ResXManager.View.Controls
+{
+    using System;
+    using System.Windows.Controls;
+
+    internal class AutoSuggestBox : ComboBox
+    {
+        private const string InternalTextBoxIdentifier = "PART_EditableTextBox";
+        private TextBox? _textBox;
+        private int _previousCaretIndex;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _textBox = GetTemplateChild(InternalTextBoxIdentifier) as TextBox;
+            if (_textBox != null)
+            {
+                _textBox.TextChanged += TextBox_TextChanged;
+            }
+
+            DropDownOpened += AutoSuggestBox_DropDownOpened;
+        }
+
+        private void AutoSuggestBox_DropDownOpened(object sender, EventArgs e)
+        {
+            if (_textBox != null)
+            {
+                _textBox.CaretIndex = _previousCaretIndex; // prevent automatic text selection on ComboBox open
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                _previousCaretIndex = textBox.CaretIndex;
+
+                if (!IsDropDownOpen)
+                {
+                    IsDropDownOpen = true;
+                }
+            }
+        }
+    }
+}
--- src/ResXManager.View/Visuals/LanguageSelectionBoxViewModel.cs
+++ src/ResXManager.View/Visuals/LanguageSelectionBoxViewModel.cs
@@ -1,27 +1,39 @@
 ﻿namespace ResXManager.View.Visuals
 {
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using System.Linq;
 
-    using TomsToolbox.Wpf;
-
     public partial class LanguageSelectionBoxViewModel : INotifyPropertyChanged
     {
+        private readonly IEnumerable<CultureInfo> _allLanguages;
+
         public LanguageSelectionBoxViewModel(IEnumerable<CultureInfo> existingLanguages)
         {
-            Languages = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            _allLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures)
                 .Where(culture => !CultureInfo.InvariantCulture.Equals(culture))
                 .Except(existingLanguages)
                 .OrderBy(culture => culture.DisplayName)
                 .ToArray();
         }
 
+        public string? LanguageFilter { get; set; }
+
         [Required]
         public CultureInfo? SelectedLanguage { get; set; }
 
-        public ICollection<CultureInfo>? Languages { get; }
+        public IEnumerable<CultureInfo> Languages
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(LanguageFilter)
+                    ? _allLanguages
+                    : _allLanguages.Where(r => r.Name.IndexOf(LanguageFilter, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                                               r.DisplayName.IndexOf(LanguageFilter, StringComparison.OrdinalIgnoreCase) >= 0);
+            }
+        }
     }
 }
