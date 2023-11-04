c0db584dd793513c39d4045442be91f61d1c7432
tom-englert <mail@tom-englert.de>
2023-11-04 18:01:09 +0100

    Avoid custom controls, use WPF-style filtering.

--- src/ResXManager.View/Visuals/LanguageSelectionBoxView.xaml.cs
+++ src/ResXManager.View/Visuals/LanguageSelectionBoxView.xaml.cs
@@ -2,10 +2,14 @@
 {
     using System;
     using System.Composition;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Windows.Controls;
 
     using ResXManager.Infrastructure;
 
     using TomsToolbox.Composition;
+    using TomsToolbox.Wpf;
     using TomsToolbox.Wpf.Composition;
     using TomsToolbox.Wpf.Composition.AttributedModel;
 
@@ -15,6 +19,9 @@
     [DataTemplate(typeof(LanguageSelectionBoxViewModel))]
     public partial class LanguageSelectionBoxView
     {
+        private TextBox? _textBox;
+        private int _caretIndex;
+
         [ImportingConstructor]
         public LanguageSelectionBoxView(IExportProvider exportProvider)
         {
@@ -28,6 +35,43 @@
             {
                 exportProvider.TraceXamlLoaderError(ex);
             }
+        }
+
+        private void ComboBox_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _textBox = ComboBox.VisualDescendants()
+                .OfType<TextBox>()
+                .FirstOrDefault();
+
+            if (_textBox is null)
+                return;
+
+            _textBox.TextChanged += TextBox_TextChanged;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _caretIndex = _textBox?.CaretIndex ?? -1;
+
+            switch (e.Changes.FirstOrDefault())
+            {
+                case null:
+                    return;
+
+                // only open dropdown on first change only, not when it manually has been closed
+                case { Offset: 0, AddedLength: 1 } when _textBox?.Text.Length == 1:
+                    ComboBox.IsDropDownOpen = true;
+                    break;
+            }
+        }
+
+        private void ComboBox_OnDropDownOpened(object sender, EventArgs e)
+        {
+            // prevent automatic text selection on ComboBox open
+            if (_textBox is null)
+                return;
+
+            _textBox.CaretIndex = _caretIndex;
         }
     }
 }
--- src/ResXManager.View/Visuals/LanguageSelectionBoxViewModel.cs
+++ src/ResXManager.View/Visuals/LanguageSelectionBoxViewModel.cs
@@ -6,34 +6,59 @@
     using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using System.Linq;
+    using System.Windows.Data;
 
-    public partial class LanguageSelectionBoxViewModel : INotifyPropertyChanged
+    using PropertyChanged;
+
+    using TomsToolbox.Wpf;
+
+    public sealed class LanguageSelectionBoxViewModel : ObservableObject
     {
-        private readonly IEnumerable<CultureInfo> _allLanguages;
+        private readonly CultureInfo[] _availableCultures;
 
         public LanguageSelectionBoxViewModel(IEnumerable<CultureInfo> existingLanguages)
         {
-            _allLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            _availableCultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
                 .Where(culture => !CultureInfo.InvariantCulture.Equals(culture))
                 .Except(existingLanguages)
                 .OrderBy(culture => culture.DisplayName)
                 .ToArray();
+
+            Languages = new ListCollectionView(_availableCultures);
         }
 
-        public string? LanguageFilter { get; set; }
+        [OnChangedMethod(nameof(OnTextChanged))]
+        public string? Text { get; set; }
+
+        private void OnTextChanged(string? oldValue, string? newValue)
+        {
+            Languages.Filter = string.IsNullOrEmpty(newValue) ? null : (item) => FilterPredicate(item as CultureInfo, newValue);
+
+            try
+            {
+                var culture = CultureInfo.GetCultureInfo(newValue);
+                if (_availableCultures.Contains(culture))
+                {
+                    SelectedLanguage = culture;
+                    return;
+                }
+            }
+            catch
+            {
+                // text is not a valued culture id
+            }
+
+            SelectedLanguage = null;
+        }
 
         [Required]
         public CultureInfo? SelectedLanguage { get; set; }
 
-        public IEnumerable<CultureInfo> Languages
-        {
-            get
-            {
-                return string.IsNullOrWhiteSpace(LanguageFilter)
-                    ? _allLanguages
-                    : _allLanguages.Where(r => r.Name.IndexOf(LanguageFilter, StringComparison.OrdinalIgnoreCase) >= 0 ||
-                                               r.DisplayName.IndexOf(LanguageFilter, StringComparison.OrdinalIgnoreCase) >= 0);
-            }
-        }
+        public ICollectionView Languages { get; }
+
+        private static bool FilterPredicate(CultureInfo? culture, string? selectedText) =>
+            culture != null
+            && (culture.Name.IndexOf(selectedText, StringComparison.OrdinalIgnoreCase) >= 0
+                || culture.DisplayName.IndexOf(selectedText, StringComparison.OrdinalIgnoreCase) >= 0);
     }
 }
