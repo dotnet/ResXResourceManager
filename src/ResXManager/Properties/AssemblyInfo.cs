2502ad2c3fe99997d11f5c481db45462d8fb1ae3
tom-englert <mail@tom-englert.de>
2023-11-04 15:39:50 +0100

    Migrate ResXManager.csproj to SDK style project

--- src/ResXManager/Properties/AssemblyInfo.cs
+++ src/ResXManager/Properties/AssemblyInfo.cs
@@ -1,30 +1,6 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
+﻿using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("tom-englert.de")]
-[assembly: AssemblyProduct("ResXManager")]
-[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors.")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyVersion(Product.Version)]
-[assembly: AssemblyFileVersion(Product.Version)]
-
-[assembly: AssemblyTitle("ResXManager")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
 [assembly: InternalsVisibleTo("ResXManager.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fbfea61559d9676195b8373508daa5755fc71550caf26ae58032ea57c651e9a97b09ab548aeb7bbb311a1df8fb57283323f9ea09aaa6751b5ac0ae5232684296ebc433ebb08e6c935ef9dd5463ce2e741748aaa8175c54fb2d7454926b35bd739f40797fae6a8c2a520a5cd77993b163b5c6ff37f70dc3ec2ae2905f2e4776dc")]
-
-internal static class Product
-{
-    public const string Version = "0.0.0.0";
-}
-
-
