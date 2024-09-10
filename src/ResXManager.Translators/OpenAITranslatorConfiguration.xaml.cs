9aab4666b419b7d78990bfeaa050f56c9c4f9b05
Attila Greguss <attila.greguss@netduma.com>
2024-09-10 16:07:04 +0100

    Add OpenAI translator

---
+++ src/ResXManager.Translators/OpenAITranslatorConfiguration.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ResXManager.Translators;
+
+using TomsToolbox.Wpf.Composition.AttributedModel;
+
+/// <summary>
+/// Interaction logic for OpenAITranslatorConfiguration.xaml
+/// </summary>
+[DataTemplate(typeof(OpenAITranslator))]
+public partial class OpenAITranslatorConfiguration
+{
+    public OpenAITranslatorConfiguration()
+    {
+        InitializeComponent();
+    }
+}
