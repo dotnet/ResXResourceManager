bc57c179157c9767d0cfcf5f30c47a5696c9db15
Rami <icnocop@users.noreply.github.com>
2025-04-24 19:49:39 -0700

    Added support to "Move to resource" for .razor files

--- src/ResXManager.VSIX.Compatibility/MoveToResourceConfiguration.cs
+++ src/ResXManager.VSIX.Compatibility/MoveToResourceConfiguration.cs
@@ -50,7 +50,7 @@ public class MoveToResourceConfiguration : ItemTrackingCollectionHost<MoveToReso
 {
     public const string Default = @"{""Items"":
 [{""Extensions"":"".cs,.vb"",""Patterns"":""$Namespace.$File.$Key|$File.$Key|StringResourceKey.$Key|$Namespace.StringResourceKey.$Key|nameof($File.$Key), ResourceType = typeof($File)|ErrorMessageResourceType = typeof($File), ErrorMessageResourceName = nameof($File.$Key)""}
-,{""Extensions"":"".cshtml,.vbhtml"",""Patterns"":""@$Namespace.$File.$Key|@$File.$Key|@StringResourceKey.$Key|@$Namespace.StringResourceKey.$Key""}
+,{""Extensions"":"".cshtml,.vbhtml,.razor"",""Patterns"":""@$Namespace.$File.$Key|@$File.$Key|@StringResourceKey.$Key|@$Namespace.StringResourceKey.$Key""}
 ,{""Extensions"":"".cpp,.c,.hxx,.h"",""Patterns"":""$File::$Key""}
 ,{""Extensions"":"".aspx,.ascx"",""Patterns"":""<%$ Resources:$File,$Key %>|<%= $File.$Key %>|<%= $Namespace.$File.$Key %>""}
 ,{""Extensions"":"".xaml"",""Patterns"":""\""{x:Static properties:$File.$Key}\""""}
