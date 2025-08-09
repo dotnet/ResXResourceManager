a75af6794456494432a6374e3987c9f33ffb4da8
tom-englert <mail@tom-englert.de>
2025-08-09 17:53:20 +0200

    Support both string format interpolation notations "${key}" and "{{key}}" for web export

---
+++ src/ResXManager.Tests/WebExporterOutput/src/index.ts
@@ -0,0 +1,22 @@
+// src/index.ts
+
+import { WebExportTest } from './resources';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const webExportTest = new WebExportTest();
+
+console.log(webExportTest.SimpleString);
+console.log(webExportTest.WithInterpolation({ voltage: 220, current: 5 }));
+console.log(webExportTest.WithBadInterpolation({ voltage: 220, current: 5 }));
+
+// Read and deserialize resources.de.json
+const resourcesPath = path.join(__dirname, 'resources.de.json');
+const resourcesData = fs.readFileSync(resourcesPath, 'utf8');
+const germanResources = JSON.parse(resourcesData);
+
+const webExportTestDE = Object.assign(new WebExportTest(), germanResources.WebExportTest);
+
+console.log(webExportTestDE.SimpleString);
+console.log(webExportTestDE.WithInterpolation({ voltage: 220, current: 5 }));
+console.log(webExportTestDE.WithBadInterpolation({ voltage: 220, current: 5 }));
---
+++ src/ResXManager.Tests/WebFilesExporterTests.cs
@@ -0,0 +1,28 @@
+﻿using ResXManager.Model;
+
+using Xunit;
+
+namespace ResXManager.Tests;
+
+public class WebFilesExporterTests
+{
+    [Theory]
+    [InlineData("Hello ${name}", new[] { "name" })]
+    [InlineData("Welcome {{user}}!", new[] { "user" })]
+    [InlineData("No placeholders here", new string[0])]
+    [InlineData("Multiple: ${first} and {{second}}", new[] { "first", "second" })]
+    [InlineData("Multiple /w space: ${ first } and {{ second }}", new[] { "first", "second" })]
+    [InlineData("Multiple too many space: ${  first } and {{ second  }}", new string[0])]
+    [InlineData("${a}${b}${a}", new[] { "a", "b" })]
+    [InlineData("Mix: ${x} {{y}} ${z}", new[] { "x", "y", "z" })]
+    [InlineData("Edge: ${ } {{ }}", new string[0])]
+    [InlineData("Nested: ${outer${inner}}", new[] { "inner" })] // Only matches 'inner' due to regex
+    public void ExtractPlaceholders_ReturnsExpectedPlaceholders(string input, string[] expected)
+    {
+        // Act
+        var result = WebFilesExporter.ExtractPlaceholders(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
