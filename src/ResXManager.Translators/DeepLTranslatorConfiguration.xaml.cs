cd478e90e06bb403d40f2ab2ce0393cfd0d73f67
Adrian Turcanu <adrian.turcanu@cometanalysis.com>
2025-11-12 16:06:09 +0200

    Add option to enable beta languages when using DeepL api

---
+++ src/ResXManager.Translators/DeepLTranslatorConfiguration.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ResXManager.Translators;
+
+using TomsToolbox.Wpf.Composition.AttributedModel;
+
+/// <summary>
+/// Interaction logic for DeepLTranslatorConfiguration.xaml
+/// </summary>
+[DataTemplate(typeof(DeepLTranslator))]
+public partial class DeepLTranslatorConfiguration
+{
+    public DeepLTranslatorConfiguration()
+    {
+        InitializeComponent();
+    }
+}
